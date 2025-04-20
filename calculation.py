6d9d2ad535f3e04c7c3167f59244d23bcb451e4c
Dennis Liu <dennisye@umich.edu>
2025-04-20 15:48:42 -0400

    turned code dennis.py into functions and added a file for calculation

---
+++ calculation.py
@@ -0,0 +1,12 @@
+from dennis import *
+
+data_list = []
+data_list = get_game_data()
+#print(data_dict)
+
+max_capacity = get_max_capacity(data_list[1])
+#print(max_capacity)
+
+create_database(data_list[0], data_list[1], max_capacity)
+
+create_graph(data_list[0], max_capacity)
