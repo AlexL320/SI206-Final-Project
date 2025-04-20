25ce4e4d87f69b8de1abf05de6fc4079aad2a001
AlexL320 <158526301+AlexL320@users.noreply.github.com>
2025-04-20 16:47:00 -0400

    Add files via upload

--- calculation.py
+++ calculation.py
@@ -1,4 +1,5 @@
 from dennis import *
+from alex_main import *
 
 data_list = ()
 data_list = get_game_data()
@@ -9,4 +10,9 @@ max_capacity = get_max_capacity(data_list[1])
 
 create_database(data_list[0], data_list[1], max_capacity)
 
-create_graph(data_list[0], max_capacity)
+im_dict = create_graph(data_list[0], max_capacity)
+
+wiki_data = get_wiki_data()[0]
+state_list = get_wiki_data()[1]
+create_location_database(wiki_data, state_list)
+create_scatter_graph(wiki_data, im_dict)
