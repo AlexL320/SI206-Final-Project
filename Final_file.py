54beaa7fc344aad8e712aea69ca06772d0f2d02d
dennycode22 <158521306+dennycode22@users.noreply.github.com>
2025-04-22 23:19:22 -0400

    Add files via upload

---
+++ Final_file.py
@@ -0,0 +1,24 @@
+from final_file import *
+from data_gather import *
+
+data_list = ()
+data_list = get_game_data()
+#print(data_list)
+
+wiki_data = get_wiki_data()[0]
+state_list = get_wiki_data()[1]
+city_dict = get_wiki_data()[2]
+
+max_capacity = get_max_capacity(city_dict)
+
+#create_database(data_list[0], city_dict, max_capacity, wiki_data, state_list)
+print(create_database(data_list[0], city_dict, max_capacity, wiki_data, state_list))
+
+create_graph()
+
+create_scatter_graph()
+
+games = game_data()
+
+conn = fetch_weather_data(games, api_key, weather_elements, max_entries)
+make_pie_chart(conn)
